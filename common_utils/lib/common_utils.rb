ce380715e660306e82ed46fd2a869aef38ae2ef5
Vish <savishy.pv@gmail.com>
2016-08-23 14:52:54 +0530

    added which command

--- common_utils/lib/common_utils.rb
+++ common_utils/lib/common_utils.rb
@@ -1,5 +1,14 @@
 class Common_utils
-	def self.hi
-		puts "Hello world!"
+	# check if command exists 
+	# http://stackoverflow.com/a/5471032/682912
+	def self.which(cmd)
+		exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+		ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+			exts.each { |ext|
+				exe = File.join(path, "#{cmd}#{ext}")
+				return exe if File.executable?(exe) && !File.directory?(exe)
+			}
+		end
+		return nil
 	end
 end
